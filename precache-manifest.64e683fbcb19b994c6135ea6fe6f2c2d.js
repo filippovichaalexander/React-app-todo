05f0c7d9d4bf5533a7b613106cb7191387479585
unknown <alex-fil@mail.ru>
2020-12-27 22:58:03 +0300

    Updates

---
+++ precache-manifest.64e683fbcb19b994c6135ea6fe6f2c2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9161751b75ed92975a310e7bcd2c1487",
+    "url": "/React-app-todo/index.html"
+  },
+  {
+    "revision": "fd386bd7ccf470e19df6",
+    "url": "/React-app-todo/static/css/main.a99200be.chunk.css"
+  },
+  {
+    "revision": "f77af3f6bcf05a2de5f6",
+    "url": "/React-app-todo/static/js/2.95bcd73a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-app-todo/static/js/2.95bcd73a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd386bd7ccf470e19df6",
+    "url": "/React-app-todo/static/js/main.79e2d297.chunk.js"
+  },
+  {
+    "revision": "027bb9de4d9027faec70",
+    "url": "/React-app-todo/static/js/runtime-main.3d9389c8.js"
+  }
+]);
